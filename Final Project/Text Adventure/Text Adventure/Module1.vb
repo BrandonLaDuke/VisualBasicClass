0da4049cbdfab09768eb658caff08d6617d7adf9
Brandon LaDuke <bjldbjld@gmail.com>
2017-01-28 10:39:32 -0500

    Game Start Sub has been added

--- Final Project/Text Adventure/Text Adventure/Module1.vb
+++ Final Project/Text Adventure/Text Adventure/Module1.vb
@@ -1,7 +1,30 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim quitGame As Boolean = 0
+        Console.WriteLine("Hello, Welcome to the Brandon LaDuke's Text Adventure Game!")
 
+        Dim play As Boolean = 0
+        While play = 0
+            Console.WriteLine("[1] Start  [2] Exit")
+            Dim start As String = Console.ReadLine()
+            If start = "1" Then
+                Console.WriteLine("GameStart")
+                GameStart()
+            ElseIf start = "2" Then
+                Console.WriteLine("Sorry to see you go so soon! Please comeback and visit!")
+                Threading.Thread.Sleep(5000)
+                Exit Sub
+            Else
+                Console.WriteLine("I don't understand what you want me to do.")
+            End If
+        End While 'Game Start Loop
+
+    End Sub
+
+    Sub GameStart()
+        Console.WriteLine()
+        Console.WriteLine("Hi from Game Start!")
     End Sub
 
 End Module
