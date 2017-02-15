0059ae1fead145ab1f5c6da859f85816427e319a
Brandon LaDuke <bjldbjld@gmail.com>
2017-02-14 19:11:08 -0500

    Finished Credits

--- Final Project/Re_Memorize/Text Adventure/Re_Memorize.vb
+++ Final Project/Re_Memorize/Text Adventure/Re_Memorize.vb
@@ -5,13 +5,16 @@
         Console.WriteLine("The Year is 2086, Memories can be uploaded, downloaded, traded, and shared.")
         Console.WriteLine("It was all safe and secure, well so we thought. Two years ago a company by the name of Memorize, located in Neo Paris, created a network that allowed for the digitising of human memories.")
         Console.WriteLine("Every thing seemed to work alright and be very benificial to the public. Untill things went wrong. Very wrong...")
-        Threading.Thread.Sleep(13000)
+        Console.WriteLine()
+        Console.WriteLine("Press Any Key to continue.")
+        Console.ReadKey()
         Console.WriteLine()
         Console.WriteLine()
         Dim play As Boolean = 0
         While play = 0
+            Console.WriteLine()
             Console.WriteLine("Welcome to Re:Memorize")
-            Threading.Thread.Sleep(3000)
+            Threading.Thread.Sleep(2000)
             Console.WriteLine()
             Console.WriteLine("[1] Start  [2] Credits [3] Exit")
             Dim start As String = Console.ReadLine()
@@ -19,7 +22,6 @@
                 Console.WriteLine("GameStart")
                 GameStart()
             ElseIf start = "2" Then
-                Console.WriteLine("---------- Credits ----------")
                 Credits()
             ElseIf start = "3" Then
                 Console.WriteLine("Thank's for playing! Please comeback and visit!")
@@ -62,6 +64,7 @@
                     numInput = Console.ReadLine()
                     Exit Select
                 Case Else
+                    Console.WriteLine()
                     Console.WriteLine("I don't understand what you meant by: " + numInput)
                     numInput = ""
                     numInput = Console.ReadLine()
@@ -81,10 +84,42 @@
     End Sub
 
     Sub MainGame()
-        Console.WriteLine("Hello from the min game!")
+        Console.WriteLine("Hello from the main game!")
     End Sub
 
     Sub Credits()
-        Console.WriteLine("Credits")
+        Console.WriteLine("------------Credits------------")
+        Console.WriteLine()
+        Console.ReadKey()
+        Console.WriteLine("Re:Memorize")
+        Console.WriteLine()
+        Console.ReadKey()
+        Console.WriteLine("Written by Brandon LaDuke")
+        Console.WriteLine()
+        Console.ReadKey()
+        Console.WriteLine("Programed by Brandon LaDuke")
+        Console.WriteLine()
+        Console.ReadKey()
+        Console.WriteLine("Based on the Game 'Remember Me' by DontNod Entertainment. Published by Capcom")
+        Console.WriteLine()
+        Console.ReadKey()
+        Console.WriteLine("Available for the PS3, Xbox 360 and PC")
+        Console.WriteLine()
+        Console.ReadKey()
+        Console.WriteLine("You can check out more of my work at: BrandonLaDuke.com")
+        Console.WriteLine()
+        Console.ReadKey()
+        Console.WriteLine("Thank you for playing!")
+        Dim creditTimer As Integer = 5
+        While creditTimer > 0
+            Threading.Thread.Sleep(1000)
+            Console.Write(".")
+            creditTimer = creditTimer - 1
+        End While
+        While creditTimer < 5
+            Console.WriteLine()
+            creditTimer = creditTimer + 1
+        End While
+
     End Sub
 End Module
