ac9036859fb1be22c75afb7536f6eb3a21713d46
Brandon LaDuke <bjldbjld@gmail.com>
2017-02-15 20:43:02 -0500

    Bug fixes

--- bladuke_distances_week7_classwork/bladuke_distances_week7_classwork/Form1.Designer.vb
+++ bladuke_distances_week7_classwork/bladuke_distances_week7_classwork/Form1.Designer.vb
@@ -132,7 +132,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(378, 283)
+        Me.ClientSize = New System.Drawing.Size(378, 219)
         Me.Controls.Add(Me.txtMiles)
         Me.Controls.Add(Me.btnShow)
         Me.Controls.Add(Me.Label7)
