c53b12430f49e1c188c3e69b33bdb02d5e63c2bc
Brandon LaDuke <bjldbjld@gmail.com>
2017-02-19 17:27:34 -0500

    Added Homework for Week 7

---
+++ bladuke_nutritional_ana_wk7_homework/bladuke_nutritional_ana_wk7_homework/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvOutput = New System.Windows.Forms.DataGridView()
+        Me.btnDetermine = New System.Windows.Forms.Button()
+        CType(Me.dgvOutput, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvOutput
+        '
+        Me.dgvOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvOutput.Location = New System.Drawing.Point(21, 77)
+        Me.dgvOutput.Name = "dgvOutput"
+        Me.dgvOutput.Size = New System.Drawing.Size(240, 150)
+        Me.dgvOutput.TabIndex = 0
+        '
+        'btnDetermine
+        '
+        Me.btnDetermine.Location = New System.Drawing.Point(21, 12)
+        Me.btnDetermine.Name = "btnDetermine"
+        Me.btnDetermine.Size = New System.Drawing.Size(240, 59)
+        Me.btnDetermine.TabIndex = 1
+        Me.btnDetermine.Text = "Determine Nutritional Contents of a Meal"
+        Me.btnDetermine.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnDetermine)
+        Me.Controls.Add(Me.dgvOutput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvOutput, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents dgvOutput As DataGridView
+    Friend WithEvents btnDetermine As Button
+End Class
---
+++ bladuke_nutritional_ana_wk7_homework/bladuke_nutritional_ana_wk7_homework/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Structure NutFact
+        Dim nutrient As String
+        Dim amount As Double
+    End Structure
+
+    Private Sub btnDetermine_Click(sender As Object, e As EventArgs) Handles btnDetermine.Click
+        Dim servings(9) As Double
+        Dim nutFacts(4) As NutFact
+
+        GetAmmounts(servings)
+        nutFacts = ProcessData(servings)
+        showData(nutFacts)
+    End Sub
+
+    Sub GetAmmounts(ByRef servings() As Double)
+        Dim foods() As String = IO.File.ReadAllLines("Foods.txt")
+        'Get the number of servings from each foods.
+        For i As Integer = 0 To 9
+            servings(i) = CDbl(InputBox("How many servings of " & foods(i)))
+        Next
+    End Sub
+
+    Function ProcessData(servings() As Double) As NutFact()
+        Dim nutrients() As String = IO.File.ReadAllLines("Nutrients.txt")
+        Dim nutTable(,) As Double = {{23, 3, 0.3, 8100, 93},
+        {160, 2, 1, 9230, 46},
+        {230, 10, 3, 120, 343},
+        {85, 8, 0, 500, 302},
+        {65, 3, 1, 0, 24},
+        {178, 3.8, 0.9, 0, 18},
+        {110, 2, 1, 2510, 30},
+        {156, 2.4, 0.4, 7000, 80},
+        {575, 126.8, 3.6, 0, 73},
+        {405, 28.8, 26.6, 984, 190}}
+        Dim nutritionFacts(4) As NutFact 'This array of structures has an element for each nutriuent
+
+        For i As Integer = 0 To 4
+            nutritionFacts(i).nutrient = nutrients(i)
+            Dim sum As Double = 0
+            For k As Integer = 0 To 9
+                sum += servings(k) * nutTable(k, i)
+            Next
+            nutritionFacts(i).amount = sum
+        Next
+        Return nutritionFacts
+    End Function
+
+    Sub ShowData(nutFacts() As NutFact)
+        Dim query = From element In nutFacts
+                    Let Nutrient = element.nutrient
+                    Let Amount = (element.amount).ToString("N")
+                    Select Nutrient, Amount
+        dgvOutput.DataSource = query.ToList
+        dgvOutput.CurrentCell = Nothing
+    End Sub
+End Class
